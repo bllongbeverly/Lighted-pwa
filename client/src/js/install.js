c6abed58b1048949bb0aaecdde657b2c06ce5c24
bllongbeverly <bllongbeverly@gmail.com>
2023-08-03 15:07:13 -0500

    add event hanlder in install.js, add scripts to package.json, installed webpack

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,32 @@
 const butInstall = document.getElementById('buttonInstall');
 
+
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+let deferredPrompt;
+
+
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    deferredPrompt = event;
+    butInstall.classList.toggle('hidden', false);
+    });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if (deferredPrompt) {
+    deferredPrompt.prompt();
+      const { outcome } = await deferredPrompt.userChoice;
+      if (outcome === 'accepted') {
+        console.log('App installed');
+      } else {
+        console.log('App not installed');
+      }
+      deferredPrompt = null;
+    }
+  });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('App installed');
+  });
