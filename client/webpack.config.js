b25b0ecc4fdfc1d712f2a35125f45366304669f3
bllongbeverly <bllongbeverly@gmail.com>
2023-08-03 14:34:47 -0500

    add css and babel to webpackconfig

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,7 +5,6 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +17,47 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        chunks: ['main'], 
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/install.html',
+        filename: 'install.html', 
+        chunks: ['install'], 
+      }),
+      new WebpackPwaManifest({
+        name: 'Your App Name',
+        short_name: 'App Name',
+        description: 'Your app description.',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: path.resolve('src/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src/js/src-sw.js', 
+       
+      }),
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.js$/, // Apply Babel to JavaScript files
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.css$/, // Apply CSS loader to CSS files
+          use: ['style-loader', 'css-loader'],
+        },
       ],
     },
   };
